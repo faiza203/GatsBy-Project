861f829238bb12a46f7e14b899f860e3f6c059da
faiza203 <fiazfaiza981@gmail.com>
2021-01-23 14:21:26 -0500

    Update Home and delete un needed files

---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        {
+          name: 'description',
+          content: 'This is a sample website for the Gatsby crash course',
+        },
+        { name: 'keywords', content: 'gatsby, react, tutorial' },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+    </div>
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
